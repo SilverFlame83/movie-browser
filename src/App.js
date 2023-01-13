f569ca231a63f47b812e74f620749cbab2ad9a14
SilverFlame83 <imren_83@abv.bg>
2023-01-13 12:36:56 +0200

    Router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
-import './App.css';
-import './components/Navbar';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import "./components/Navbar";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-   <h1>It's working</h1>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={About}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import capture from '../assets/capture.jpg';
+
+const Home = () => {
+return <img src={capture} alt='movie'/>
+};
+
+export default Home;
