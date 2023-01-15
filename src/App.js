2301b468d780f70be60ca3898ad77d076168efe8
SilverFlame83 <imren_83@abv.bg>
2023-01-15 17:45:50 +0200

    Fetching data is implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import About from "./components/About";
@@ -8,13 +9,33 @@ import Navbar from "./components/Navbar";
 import SearchView from "./components/SearchView";
 
 function App() {
+  const [searchResult, setSearchResult] = useState("Hello from App");
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=${searchText}&page=1&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const http= data.results.map(data => data.original_title)
+        setSearchResult(http)});
+    }
+  }, [searchText]);
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar searchText={searchText} setSearchText={setSearchText} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/search" element={<SearchView />} />
+        <Route path="/about" element={<About/>} />
+        <Route
+          path="/search"
+          element={
+            <SearchView keyword={searchText} searchResult={searchResult} />
+          }
+        />
       </Routes>
     </div>
   );
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,7 +1,16 @@
-const SearchView = ()=>{
-    return <div>
-        <p>This is search page...</p>
-    </div>
-}
+import Hero from "./Hero";
+
+//https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=star%wars&page=1&include_adult=false
+const SearchView = ({ keyword, searchResult }) => {
+  const title = `This is my search ${keyword}`;
+  
+
+  return (
+    <>
+      <Hero text={title} />
+      {searchResult}
+    </>
+  );
+};
 
 export default SearchView;
