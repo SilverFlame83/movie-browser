da65e9dd6994e6bfc20221512bc9aa5c748886c8
SilverFlame83 <imren_83@abv.bg>
2023-01-18 20:51:35 +0200

    Loading added

--- src/components/MoviesView.js
+++ src/components/MoviesView.js
@@ -1,17 +1,38 @@
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Hero from './Hero'
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Hero from "./Hero";
 
-const MoviesView = ()=>{
-    const {id} = useParams();
-    const [movieDetails, setMovieDetails] = useState({});
+const MoviesView = () => {
+  const { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        console.log('Id is changed', id)
-    }, [id])
+  useEffect(() => {
+    //https://api.themoviedb.org/3/movie/11?api_key=ab166ff82684910ae3565621aea04d62&language=en-US
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=ab166ff82684910ae3565621aea04d62&language=en-US`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeout(()=>{
+            setMovieDetails(data);
+          setIsLoading(false); 
+        }, 2000)
+      });
+  }, [id]);
 
+  function movieDetailsView (){
+    if (isLoading){
+       return <Hero text='Loading...'/>
+    }
+    if (!isLoading){
+        return <Hero text={movieDetails.original_title}/>
+    }
+  }
 
-    return <Hero text='Movie detail view'/>
-}
+  return <>
+    {movieDetailsView()}
+  </>
+};
 
 export default MoviesView;
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -3,30 +3,32 @@ import { Link } from "react-router-dom";
 import Hero from "./Hero";
 
 //https://api.themoviedb.org/3/search/movie?api_key=ab166ff82684910ae3565621aea04d62&language=en-US&query=star%wars&page=1&include_adult=false
-const SearchView = ({ keyword, searchResult }) => {
-  const MovieCard = ({ movie }) => {
-    const dataUrl= movie.id
-    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    return (
-      <>
-        <div className="col-lg-3 col-md-3 col-2 my-4">
-          <div className="card">
-            <img
-              src={posterUrl}
-              className="card-img-top"
-              alt={movie.original_title}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{movie.original_title}</h5>
-              <Link to={`/movies/${dataUrl}`} className="btn btn-primary">
-                Show details
-              </Link>
-            </div>
+const MovieCard = ({ movie }) => {
+  const detailUrl= `/movies/${movie.id}`
+  const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  return (
+    <>
+      <div className="col-lg-3 col-md-3 col-2 my-4">
+        <div className="card">
+          <img
+            src={posterUrl}
+            className="card-img-top"
+            alt={movie.original_title}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{movie.original_title}</h5>
+            <Link to={detailUrl} className="btn btn-primary">
+              Show details
+            </Link>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
+
+
+const SearchView = ({ keyword, searchResult }) => {
 
   const resultsHtml = searchResult.map((obj, i) => {
     return <MovieCard movie={obj} key={i} />;
